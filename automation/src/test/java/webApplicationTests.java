ae146a8b9e9e6515a206b97ca0040b7074b87c44
Adam A <adam.aharony@gmail.com>
2022-10-07 15:45:42 +0300

    edited

--- automation/src/test/java/webApplicationTests.java
+++ automation/src/test/java/webApplicationTests.java
@@ -1,9 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,52 +9,53 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class webApplicationTests {
-    public ChromeDriver driver;
-    public ChromeOptions opts;
-    public String SITE = "http://localhost";
-    public int PORT = 800;
-    public String TITLE = "HIT - DevOps course";
-    public String NAME = "Adam Aharony";
-
-    @BeforeEach
-    public void setup() {
-        opts = new ChromeOptions();
-        opts.addArguments("--headless");
-        opts.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        opts.addArguments("--no-sandbox"); // Bypass OS security model
-        opts.addArguments("--whitelisted-ips=\"\"");
-        System.setProperty("webdriver.chrome.whitelistedIps", "");
-        driver = new ChromeDriver(opts);
-        // Navigate to site
-        driver.get(String.format("%s:%s", SITE, Integer.toString(PORT)));
-    }
-
-    @AfterEach
-    public void quit() {
-        driver.quit();
-    }
-
     @Test
     public void testHTTPResponseCode() {
-        RestAssured.baseURI = SITE;
-        RestAssured.port = PORT;
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--no-sandbox"); 
+        options.addArguments("--whitelisted-ips=\"\"");
+        System.setProperty("webdriver.chrome.whitelistedIps", "");
+        ChromeDriver driver = new ChromeDriver(options);
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 800;
 
         RequestSpecification HTTPRequest = RestAssured.given();
         Response res = HTTPRequest.get();
-        int actualCode = res.getStatusCode();
-
-        Assertions.assertEquals(200, actualCode, "Correct code returned");
+        int obtainedCode = res.getStatusCode();
+        driver.quit();
+        
+        Assertions.assertEquals(200, obtainedCode, "Correct code returned");
     }
 
     @Test
     public void testWebApplicationTitle() {
-        String actualTitle = driver.getTitle();
-        Assertions.assertEquals(TITLE, actualTitle, "Correct title returned");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--no-sandbox"); 
+        options.addArguments("--whitelisted-ips=\"\"");
+        System.setProperty("webdriver.chrome.whitelistedIps", "");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("http://localhost:8000");
+        String obtainedTitle = driver.getTitle();
+        driver.quit();
+        Assertions.assertEquals("HIT - DevOps course", obtainedTitle);
     }
 
     @Test
     public void testSiteUI() {
-        String actualName = driver.findElement(By.xpath("/html/body/h2[3]")).getText();
-        Assertions.assertEquals(NAME, actualName, "Correct name returned");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--no-sandbox"); 
+        options.addArguments("--whitelisted-ips=\"\"");
+        System.setProperty("webdriver.chrome.whitelistedIps", "");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("http://localhost:8000");
+        String obtainedName = driver.findElement(By.xpath("/html/body/h2[3]")).getText();
+        driver.quit();
+        Assertions.assertEquals("Daniel Weinermann", obtainedName);
     }
 }
