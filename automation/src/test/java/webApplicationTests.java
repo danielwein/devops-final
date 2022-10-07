9e0e4f274d5b50e1679a3f3862383d57458c5724
Adam A <adam.aharony@gmail.com>
2022-10-07 15:33:40 +0300

    Update webApplicationTests.java

--- automation/src/test/java/webApplicationTests.java
+++ automation/src/test/java/webApplicationTests.java
@@ -5,24 +5,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class webApplicationTests {
     @Test
     public void testHTTPResponseCode() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--no-sandbox"); 
+        options.addArguments("--whitelisted-ips=\"\"");
+        System.setProperty("webdriver.chrome.whitelistedIps", "");
+        ChromeDriver driver = new ChromeDriver(options);
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 800;
 
         RequestSpecification HTTPRequest = RestAssured.given();
         Response res = HTTPRequest.get();
         int obtainedCode = res.getStatusCode();
-
+        driver.quit();
+        
         Assertions.assertEquals(200, obtainedCode, "Correct code returned");
     }
 
     @Test
     public void testWebApplicationTitle() {
-        ChromeDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--no-sandbox"); 
+        options.addArguments("--whitelisted-ips=\"\"");
+        System.setProperty("webdriver.chrome.whitelistedIps", "");
+        ChromeDriver driver = new ChromeDriver(options);
         driver.get("http://localhost:8000");
         String obtainedTitle = driver.getTitle();
         driver.quit();
@@ -31,7 +46,13 @@ public class webApplicationTests {
 
     @Test
     public void testSiteUI() {
-        ChromeDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage"); 
+        options.addArguments("--no-sandbox"); 
+        options.addArguments("--whitelisted-ips=\"\"");
+        System.setProperty("webdriver.chrome.whitelistedIps", "");
+        ChromeDriver driver = new ChromeDriver(options);
         driver.get("http://localhost:8000");
         String obtainedName = driver.findElement(By.xpath("/html/body/h2[3]")).getText();
         driver.quit();
